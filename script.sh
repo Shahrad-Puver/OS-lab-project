da8c41c6a359c3c868ccb2e19c058d45bcb660a9
Shahrad Puver <gazni.shahrad@gmail.com>
2025-07-28 03:53:47 -0400

    Adding the arguments checking code block
    The script should receive 3 arguments when being executed

--- script.sh
+++ script.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "This is a test code output written for testing purposes!"
+
+# OS lab project
+# by Shahrad-Puver (Shahrad Gazni)
+
+# Check for required arguments
+# We require the user to give the script 3 arguments at the time of execution;
+# If the number of received arguments is less than than, the script should abort.
+if [ $# -ne 3 ]; then
+	echo "Usage: $0 <source_path> <file_extension> <backup_path>"
+	exit 1
+fi
